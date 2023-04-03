5c5f99f1e447b8e579306ba1cb083d3da46e3436
samiunblack <samiunblack@gmail.com>
2023-04-03 20:24:33 +0600

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # phitron
+
+## Course 1: Introduction to Programming Language & Problem Solving Part-I
+### Week 1: Basic Syntax, Condition & Loop
+- Data types and limitations
+- Operators, Conditional Statement
+- Loop & Nested Loop
--- module 1/input.c
+++ module 1/input.c
@@ -3,7 +3,9 @@
 int main()
 {
     int a, b;
-    scanf("%d%% %d%%", &a, &b);
-    printf("%d%% %d%%", a, b);
+    double c, d;
+    char g, h;
+    scanf("%d %d %lf %lf %c %c", &a, &b, &c, &d, &g, &h);
+    printf("%d %d, %0.2lf %0.2lf, %c %c", a, b, c, d, g, h);
     return 0;
 }
