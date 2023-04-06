dfaaf28325a545f19b468b26cb0b78da9dc3f4bc
samiunblack <samiunblack@gmail.com>
2023-04-06 20:40:39 +0600

    gitignore

--- course 1/practice_1/problem_1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Recently I heard that you've achieved 95%% marks in your exam.\nThis is brilliant!\nI wish you'll shine in your life!\tGood luck with all the barriers(/\\) in your life.");
-    return 0;
-}
