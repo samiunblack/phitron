82648a3f371deab0433e50087ae9474eef05cfc7
samiunblack <samiunblack@gmail.com>
2023-04-20 18:52:59 +0600

    module 10.5

---
+++ course 1/module 10.5/strings.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h> 
+ 
+int main() {
+    char a[11];
+    char b[11];
+ 
+    scanf("%s %s", a, b);
+ 
+    int aLen = strlen(a);
+    int bLen = strlen(b);
+ 
+    printf("%d %d\n", aLen, bLen);
+ 
+    printf("%s%s\n", a, b);
+ 
+    char temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+ 
+    printf("%s %s", a, b);
+ 
+    return 0;
+}
