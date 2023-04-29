5527d175142f09e58555b760beff43d8f200a693
samiunblack <samiunblack@gmail.com>
2023-04-30 01:22:13 +0600

    module 13

--- course 1/module 13/selection_sort.c
+++ course 1/module 13/selection_sort.c
@@ -13,9 +13,9 @@ int main() {
 
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = i + 1; j < n; j++)
         {
-            if(arr[i] < arr[j])
+            if(arr[i] > arr[j])
             {
                 int temp = arr[i];
                 arr[i] = arr[j];
