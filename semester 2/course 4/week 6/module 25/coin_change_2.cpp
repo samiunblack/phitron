fb2fd76c186171f036aa5922edec0fd22391d83c
samiunblack <samiunblack@gmail.com>
2023-09-13 15:04:49 +0600

    course 4 week 6 revisit

--- semester 2/course 4/week 6/module 25/coin_change_2.cpp
+++ semester 2/course 4/week 6/module 25/coin_change_2.cpp
@@ -1,60 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int mn = INT_MAX;
-int dp[1005][1005];
-
-int knapsack_count(int n, int coin[], int s, int cnt)
-{
-    if(n == 0 || s == 0)
-    {
-        if(s == 0)
-        {
-            mn = min(cnt, mn);
-            return mn;
-        }
-        else return mn;
-    }
-
-    if(dp[n][s] != -1) return dp[n][s];
-
-    if(coin[n - 1] <= s)
-    {
-        int op1 = knapsack_count(n, coin, s - coin[n - 1], cnt + 1);
-        int op2 = knapsack_count(n - 1, coin, s, cnt + 1);
-
-        return dp[n][s] = mn;
-    }
-
-    else
-    {
-        return dp[n][s] = knapsack_count(n - 1, coin, s, cnt);
-    }
-}
-
 int main()
 {
     int n;
-    
     cin >> n;
+    
+    int w[n];
 
-    int coin[n];
-
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cin >> coin[i];
+        cin >> w[i];
     }
 
     int s;
     cin >> s;
 
-    for(int i = 0; i <= n; i++)
+    int dp[n + 1][s + 1];
+
+    dp[0][0] = 0;
+    for (int i = 1; i <= s; i++) dp[0][i] = INT_MAX - 1;
+
+
+    for (int i = 1; i <= n; i++)
     {
-        for(int j = 0; j <= s; j++)
+        for (int j = 0; j <= s; j++)
         {
-            dp[i][j] = -1;
+            if (w[i - 1] <= j)
+            {
+                dp[i][j] = min(1 + dp[i][j - w[i - 1]], dp[i - 1][j]);
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
         }
     }
 
-    cout << knapsack_count(n, coin, s, 0);
+
+    if (dp[n][s] == INT_MAX - 1) cout << "Not Possible" << endl;
+    else cout << dp[n][s] << endl;
 }
