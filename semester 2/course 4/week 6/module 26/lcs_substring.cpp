08551a74120b041687f3cb9ab48001b4cf4454b4
samiunblack <samiunblack@gmail.com>
2023-09-13 15:13:05 +0600

    module 26

--- semester 2/course 4/week 6/module 26/lcs_substring.cpp
+++ semester 2/course 4/week 6/module 26/lcs_substring.cpp
@@ -13,7 +13,7 @@ int main()
 
     for(int i = 0; i <= n; i++)
     {
-        for(int j = 0; j <= n; j++)
+        for(int j = 0; j <= m; j++)
         {
             if(i == 0 || j == 0)
             {
