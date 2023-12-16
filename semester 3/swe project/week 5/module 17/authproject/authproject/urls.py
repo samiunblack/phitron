fc8964be52565a99ae9c831e5174ad13d5b2ffbb
samiunblack <samiunblack@gmail.com>
2023-12-16 18:07:46 +0600

    module 17 week 5 swe projects

--- semester 3/swe project/week 5/module 17/authproject/authproject/urls.py
+++ semester 3/swe project/week 5/module 17/authproject/authproject/urls.py
@@ -15,17 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from authapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('signup/', views.signup, name="signup"),
-    path('login/', views.login_req, name="login"),
-    path('logout/', views.logout_req, name="logout"),
-    path('profile/', views.profile, name="profile"),
-    path('reset_password/', views.reset_pass, name="reset_pass"),
-    path('forgot_password/', views.forgot_pass, name="forgot_pass"),
-    path('edit_profile/', views.edit_profile, name="edit_profile"),
+    path('', include("authapp.urls")),    
 ]
